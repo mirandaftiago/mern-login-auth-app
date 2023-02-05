bd9f810dbe50598319ca215e141c573b19a95075
Tiago Miranda <miranda.f.tiago@gmail.com>
2023-02-05 00:42:22 +0000

    register route

---
+++ routes/api/users.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// register and login validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
+// User model
+const User = require('../../models/User');
+
+router.get('/', (req, res) => {
+    res.status(200).json('Get route successfully created');
+});
+
+// POST api/users/register
+// User Registration
+router.post('/register', (req, res) => {
+    // form validation
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    // check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    };
+    
+    User.findOne({
+        email: req.body.email
+    }).then( user => {
+        if (user) {
+            return res.status(400).json({
+                email: 'Email already exists'
+            });
+        } else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+
+            // hash password before persist on database
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+});
+
+module.exports = router;
