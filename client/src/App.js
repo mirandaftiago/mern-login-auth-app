6e552455cc6a6837e03288674c09ab2a5a85e096
Tiago Miranda <miranda.f.tiago@gmail.com>
2023-02-08 16:40:43 +0000

    Materialize.css, Navbar and Landing page components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
-import './App.css';
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>   
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+      </div>
+    );
+  }
 }
 
 export default App;
