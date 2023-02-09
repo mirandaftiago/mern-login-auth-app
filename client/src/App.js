d60a9a5b1146bfc87e47b1c53f3767c9c7a8a7b1
Tiago Miranda <miranda.f.tiago@gmail.com>
2023-02-09 11:45:59 +0000

    Login and Register Authentication components

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
-import React, { Component } from "react";
-import "./App.css";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-      </div>
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <Routes>
+              <Route exact path='/' element={<Landing />} />
+              <Route exact path='/register' element={<Register />} />
+              <Route exact path='/login' element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
-
 export default App;
