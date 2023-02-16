f83e15dd89fe062e3e515749e722643d935cac38
Tiago Miranda <miranda.f.tiago@gmail.com>
2023-02-16 11:11:17 +0000

    Functional Register and Login component to fix useHistory hook deprecation

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,151 +1,140 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authActions';
 import classNames from 'classnames';
-import withRouter from '../../utils/withRouter';
+import { withRouter } from '../../utils/withRouter';
 
-class Register extends Component {
-    constructor() {
-        super();
-        this.state = {
-            name: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-            errors: {}
-        };
-    }
+const Register = (props) => {
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        errors: {}
+    });
 
-    componentDidMount() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
         // If logged in and user navigates to Register page, should redirect them to dashboard
-        if (this.props.auth.isAuthenticated) {
-          this.props.history.push("/dashboard");
+        if (props.auth.isAuthenticated) {
+            navigate('/dashboard');
         }
-    }
+    }, [props.auth.isAuthenticated, navigate]);
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.errors) {
-            this.setState({
-                errors: nextProps.errors
-            });
-        }
-    }
-
-    onChange = e => {
-        this.setState({ [e.target.id]: e.target.value });
+    const onChange = e => {
+        setState({ ...state, [e.target.id]: e.target.value });
     };
 
-    onSubmit = e => {
+    const onSubmit = e => {
         e.preventDefault();
 
         const newUser = {
-            name: this.state.name,
-            email: this.state.email,
-            password: this.state.password,
-            confirmPassword: this.state.confirmPassword
+            name: state.name,
+            email: state.email,
+            password: state.password,
+            confirmPassword: state.confirmPassword
         };
-        this.props.registerUser(newUser, this.props.history);
+        props.registerUser(newUser, props.history);
     };
 
-    render() {
-        const { errors } = this.state;
+    const { errors } = state;
 
-        return(
-            <div className='container'>
-                <div className='row'>
-                    <div className='col s8 offset-s2'>
-                        <Link to='/' className='btn-flat waves-effect'>
-                            <i className='material-icons left'>keyboard_backspace</i> Back to home
-                        </Link>
-                        <div className='col s12' style={{ paddingLeft: '11.25px' }}>
-                            <h4>
-                                <b>Register</b> below
-                            </h4>
-                            <p className='grey-text text-darken-1'>
-                                Already have an account?
-                                <Link to='/login'>Log in</Link>
-                            </p>
-                        </div>
-                        <form noValidate onSubmit={this.onSubmit}>
-                            <div className='input-field col s12'>
-                                <input 
-                                    onChange={this.onChange}
-                                    value={this.state.name}
-                                    error={errors.name}
-                                    id='name'
-                                    type='text'
-                                    className={classNames('', {
-                                        invalid: errors.name
-                                    })}
-                                />
-                                <label htmlFor='name'>Name</label>
-                                <span className='red-text'>{errors.name}</span>
-                            </div>
-                            <div className='input-field col s12'>
-                                <input
-                                    onChange={this.onChange}
-                                    value={this.state.email}
-                                    error={errors.email}
-                                    id='email'
-                                    type='email'
-                                    className={classNames('', {
-                                        invalid: errors.email
-                                    })}
-                                />
-                                <label htmlFor='email'>Email</label>
-                                <span className='red-text'>{errors.email}</span>
-                            </div>
-                            <div className='input-field col s12'>
-                                <input
-                                    onChange={this.onChange}
-                                    value={this.state.password}
-                                    error={errors.password}
-                                    id='password'
-                                    type='password'
-                                    className={classNames('', {
-                                        invalid: errors.password
-                                    })}
-                                />
-                                <label htmlFor='password'>Password</label>
-                                <span className='red-text'>{errors.password}</span>
-                            </div>
-                            <div className='input-field col s12'>
-                                <input
-                                    onChange={this.onChange}
-                                    value={this.state.confirmPassword}
-                                    error={errors.confirmPassword}
-                                    id='confirmPassword'
-                                    type='password'
-                                    className={classNames('', {
-                                        invalid: errors.confirmPassword
-                                    })}
-                                />
-                                <label htmlFor='confirmPassword'>Confirm Password</label>
-                                <span className='red-text'>{errors.confirmPassword}</span>
-                            </div>
-                            <div className='col s12' style={{ paddingLeft: '11.250px' }}>
-                                <button
-                                    style={{
-                                        width: '150px',
-                                        borderRadius: '3px',
-                                        letterSpacing: '1.5px',
-                                        marginTop: '1rem'
-                                    }}
-                                    type='submit'
-                                    className='btn btn-large waves-effect waves-light hoverable blue accent-3'
-                                    >
-                                    Sign up
-                                </button>
-                            </div>
-                        </form>
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col s8 offset-s2'>
+                    <Link to='/' className='btn-flat waves-effect'>
+                        <i className='material-icons left'>keyboard_backspace</i> Back to home
+                    </Link>
+                    <div className='col s12' style={{ paddingLeft: '11.25px' }}>
+                        <h4>
+                            <b>Register</b> below
+                        </h4>
+                        <p className='grey-text text-darken-1'>
+                            Already have an account?
+                            <Link to='/login'>Log in</Link>
+                        </p>
                     </div>
+                    <form noValidate onSubmit={onSubmit}>
+                        <div className='input-field col s12'>
+                            <input 
+                                onChange={onChange}
+                                value={state.name}
+                                error={errors.name}
+                                id='name'
+                                type='text'
+                                className={classNames('', {
+                                    invalid: errors.name
+                                })}
+                            />
+                            <label htmlFor='name'>Name</label>
+                            <span className='red-text'>{errors.name}</span>
+                        </div>
+                        <div className='input-field col s12'>
+                            <input
+                                onChange={onChange}
+                                value={state.email}
+                                error={errors.email}
+                                id='email'
+                                type='email'
+                                className={classNames('', {
+                                    invalid: errors.email
+                                })}
+                            />
+                            <label htmlFor='email'>Email</label>
+                            <span className='red-text'>{errors.email}</span>
+                        </div>
+                        <div className='input-field col s12'>
+                            <input
+                                onChange={onChange}
+                                value={state.password}
+                                error={errors.password}
+                                id='password'
+                                type='password'
+                                className={classNames('', {
+                                    invalid: errors.password
+                                })}
+                            />
+                            <label htmlFor='password'>Password</label>
+                            <span className='red-text'>{errors.password}</span>
+                        </div>
+                        <div className='input-field col s12'>
+                            <input
+                                onChange={onChange}
+                                value={state.confirmPassword}
+                                error={errors.confirmPassword}
+                                id='confirmPassword'
+                                type='password'
+                                className={classNames('', {
+                                    invalid: errors.confirmPassword
+                                })}
+                            />
+                            <label htmlFor='confirmPassword'>Confirm Password</label>
+                            <span className='red-text'>{errors.confirmPassword}</span>
+                        </div>
+                        <div className='col s12' style={{ paddingLeft: '11.250px' }}>
+                            <button
+                                style={{
+                                    width: '150px',
+                                    borderRadius: '3px',
+                                    letterSpacing: '1.5px',
+                                    marginTop: '1rem'
+                                }}
+                                type='submit'
+                                className='btn btn-large waves-effect waves-light hoverable blue accent-3'
+                                >
+                                Sign up
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
-        )
-    }
-}
+        </div>
+    );
+};
 
 Register.propTypes = {
     registerUser: PropTypes.func.isRequired,
@@ -161,4 +150,4 @@ const mapStateToProps = state => ({
 export default connect(
     mapStateToProps,
     { registerUser}
-) (withRouter(Register));
+)(withRouter(Register));
